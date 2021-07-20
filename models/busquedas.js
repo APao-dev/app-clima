181bbf12539cd468c19c05daab8d0b500e4e12e7
Paola <apaofernandez10@gmail.com>
2021-07-20 01:08:41 -0300

    instanciando axios

--- models/busquedas.js
+++ models/busquedas.js
@@ -19,10 +19,18 @@ class Busquedas {
         }
     }
 
+    get paramsWeather() {
+        return{
+            appid: process.env.OPENWEATHER_KEY,
+            units: 'metric',
+            lang: 'es'
+           
+        }
+    }
+
 // primer método que me sirve para buscar una ciudad
 // método petición asíncrona
     async ciudad(lugar = '') {
-
 
         try{
             // petición http
@@ -33,7 +41,13 @@ class Busquedas {
 
             const resp = await instance.get();
             // con este llamado se imprime el objeto de javascript
-            console.log(resp.data);
+            return resp.data.features.map(lugar => ({
+                id:lugar.id,
+                nombre: lugar.place_name,
+                lng: lugar.center[0],
+                lat: lugar.center[1],
+
+            }))
 
             // retornar los lugares que se busquen
             return []
@@ -43,6 +57,32 @@ class Busquedas {
         }
     }
 
+
+    async climaLugar(lat, lon) {
+
+        try {
+            // instancia de axios
+            const instance = axios.create({
+                baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+                params: {...this.paramsWeather, lat, lon} 
+
+            }) 
+            // respuesta
+            const resp = await instance.get();
+            const {weather, main} = resp.data;
+
+            return{
+                desc: weather[0].description,
+                min: main.temp_min,
+                max: main.temp_max,
+                temp: main.temp
+            }
+            
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
 
 
 }
