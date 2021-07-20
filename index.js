c6ecd5c89ff70627757d0781a3ff7cc0410bd0f3
Paola <apaofernandez10@gmail.com>
2021-07-20 01:07:27 -0300

    datos del clima

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const { leerInput, inquirerMenu, pausa, listarLugares } = require("./helpers/inquirer");
 const Busquedas = require("./models/busquedas");
 
 // console.log(process.env.MAPBOX_KEY);
@@ -18,23 +18,31 @@ const main = async() => {
         switch (opt) {
             case '1':
                 // Mostrar mensaje 
-                const lugar = await leerInput('Ciudad: ');
-                await busquedas.ciudad(lugar);
-
-                // Buscar el lugar
-
+                const termino = await leerInput('Ciudad: ');
+                // Buscar lugares
+                const lugares = await busquedas.ciudad(termino);
+                
                 // Seleccionar el lugar
+                const id = await listarLugares(lugares);
+                const lugarSel = lugares.find(l => l.id === id);
+               
+
+                // console.log({id});
 
                 // Datos del clima
+                const clima = await busquedas.climaLugar(lugarSel.lat, lugarSel.lng);
+               
 
                 // Mostrar Resultados
+                console.clear();
                 console.log('\nInformación de la Ciudad\n'.green);
-                console.log('Ciudad:', );
-                console.log('Latitud:',);
-                console.log('Lng:',);
-                console.log('Temperatura:',);
-                console.log('Mínima:',);
-                console.log('Máxima:',);
+                console.log('Ciudad:', lugarSel.nombre.green);
+                console.log('Lat:', lugarSel.lat);
+                console.log('Lng:', lugarSel.lng);
+                console.log('Temperatura:', clima.temp);
+                console.log('Mínima:', clima.min);
+                console.log('Máxima:',clima.max);
+                console.log('Cómo está el clima:', clima.desc.green);
             break;
     
         }
